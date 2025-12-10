afcfa77292896cb4bfa8a46d79ce6376aaeb4f1c
kaylaato <kaylalistenstomusic@gmail.com>
2025-12-10 10:31:57 -0800

    Refactor Otter class constructors and methods
    Updated constructor and added unique behavior method.

--- Otter.java
+++ Otter.java
@@ -1,10 +1,9 @@
 import java.util.Random;
-
 /**
  * The Otter class represents a virtual pet otter with attributes for health,
  * energy, and happiness. It provides methods to simulate various actions
  * the otter can perform, such as eating, sleeping, playing, swimming, and doing nothing.
- * 
+ *
  * @author Kayla To
  * @collaborators Copilot
  * @version 12/8/25
@@ -14,64 +13,83 @@ public class Otter implements Animal {
     private int energy;
     private int happiness;
     private String name;
-
+   
     /**
-     * Makes the otter eat, increasing energy and health.
-     */
-    public Otter(String name, int health, int energy, int happiness) {
-        this.name = name;
+    * Constructs an Otter with the specified attributes.
+    *
+    * @param health The initial health of the otter.
+    * @param energy The initial energy of the otter.
+    * @param happiness The initial happiness of the otter.
+    * @param name The name of the otter.
+    */
+    public Otter(int health, int energy, int happiness, String name) {
         this.health = health;
         this.energy = energy;
         this.happiness = happiness;
+        this.name = name;
     }
+
+
+    /**
+     * Constructs an Otter with the given name and initializes its attributes.
+     *
+     * @param name The name of the otter.
+     */
+    public Otter(String name) {
+        this.name = name;
+        this.health = 100;
+        this.energy = 100;
+        this.happiness = 100;
+    }
+
 
     /**
      * Makes the otter hangout with family, increasing happiness but decreasing energy.
      */
-    public void familyTime() {
+    public void uniqueBehavior() {
         System.out.println(name + " is spending time with family.");
         happiness += 15;
         energy -= 5;
     }
 
+
     /**
      * Makes the otter eat, increasing energy and health.
      */
-    @Override
     public void eat() {
         System.out.println(name + " is eating.");
         energy += 10;
         health += 5;
     }
 
+
     /**
      * Makes the otter sleep, increasing energy and happiness.
      */
-    @Override
     public void sleep() {
         System.out.println(name + " is sleeping.");
         energy += 20;
         happiness += 5;
     }
 
+
     /**
      * Makes the otter play, increasing happiness but decreasing energy.
      */
-    @Override
     public void play() {
         System.out.println(name + " is playing.");
         energy -= 15;
         happiness += 20;
     }
 
+
     /**
      * Makes the otter swim, with a chance of finding a friend or getting injured.
      */
-    @Override
     public void swim() {
         System.out.println(name + " is swimming.");
         Random rand = new Random();
-        int n = rand.nextInt(2); 
+        int n = rand.nextInt(2);
         if (n == 0){
             System.out.println(name + " found a friend while swimming!");
             happiness += 15;
@@ -83,31 +101,13 @@ public class Otter implements Animal {
         }
     }
 
+
     /**
      * Makes the otter do nothing, slightly decreasing energy.
      */
-    @Override
     public void nothing() {
         System.out.println(name + " is doing nothing...");
         energy -= 2;
     }
-<<<<<<< HEAD
-    
-=======
-
->>>>>>> 9f0d222719bccdc133eb766d2110eb17957a2709
-    /**
-     * Displays the current status of the penguin, including its name, health, energy, and happiness.
-     */
-    public void displayStatus() {
-        System.out.println("Name: " + name);
-        System.out.println("Health: " + health);
-        System.out.println("Energy: " + energy);
-        System.out.println("Happiness: " + happiness);
-    }
-<<<<<<< HEAD
-}
-=======
 }
 
->>>>>>> 9f0d222719bccdc133eb766d2110eb17957a2709
