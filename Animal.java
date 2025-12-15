de60e45be749c9fdf8ef69f2cb25ac562122a649
giannaRR <giannalistenstomusic655@gmail.com>
2025-12-15 10:42:22 -0800

    Update Animal.java

--- Animal.java
+++ Animal.java
@@ -1,3 +1,9 @@
+/**
+ * @author Kayla To
+ * @version 12/15/25
+ * Represents a generic Animal with basic behaviors and status attributes.
+ * Classes that implement this interface define how the animal behaves
+ */
 public interface Animal {
     public void eat();
     public void sleep();
@@ -10,4 +16,5 @@ public interface Animal {
     public int getHappiness();
     public void displayStatus();
 }
+
 
